b3dc1ed24757d68d29406250a94cfb156fec23c9
Konstantin <troshinkv@mail.ru>
2025-07-28 11:13:55 +1000

    Безопасность в облачных провайдерах

---
+++ terraform3/variable.tf
@@ -0,0 +1,15 @@
+
+variable "cloud_id" {
+  type        = string
+  default     = "b1g8lmno7t06ve55gp10"
+}
+
+variable "folder_id" {
+  type        = string
+  default     = "b1g8bp1nfdm701n4tb5g"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+}
